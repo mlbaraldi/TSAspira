10021150a53953eb721f76cc8b33ab87d61e4e35
Marcelo Baraldi <markbass1@hotmail.com>
2020-11-20 17:52:13 -0300

    adicionado edition na classe book

--- src/entities/Book.ts
+++ src/entities/Book.ts
@@ -3,11 +3,13 @@ import Person from "./Person"
 
 class Book extends Document{
     isbn: number;
+    edition: number;
     volume: number;
     
-    constructor(isbn:number, volume:number, title: string, subtitle: string, author: Person, publishedAt: Date) {
+    constructor(isbn:number, edition: number, volume:number, title: string, subtitle: string, author: Person, publishedAt: Date) {
         super(title, subtitle, author, publishedAt)
         this.isbn = isbn
+        this.edition = edition
         this.volume = volume
         this.title = title
         this.author = author
